fe1e6ca279ec0d492e44490c452276c1bfcce8db
zhangyang <px.zhangyang@gmail.com>
2014-11-25 10:58:07 +0800

    add templates

--- mysite/settings.py
+++ mysite/settings.py
@@ -77,18 +77,18 @@ MEDIA_URL = 'http://lpgray-media.stor.sinaapp.com/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = ROOT_PATH + '/public'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-# STATIC_URL = 'http://lpgray-static.stor.sinaapp.com'
+STATIC_URL = '/public'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    ROOT_PATH + '/static',
+    # ROOT_PATH + '/public',
 )
 # List of finder classes that know how to find static files in
 # various locations.
